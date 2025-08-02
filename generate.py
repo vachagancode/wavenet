Add gradient clipping | Switch to AdamW | Fix Accuracy calculation code

generate.py
@@ -1,28 +1,43 @@
+import os
 import torch
 import torchaudio
 
+from utils import save_generated_audio
 from config import get_config
 from wavenet import create_wavenet
 
-def generate_audio():
-    pass
+def generate_audio(model_name, model_dir, input_file=None):
+    config = get_config()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    for ix, model_config in enumerate(config):
+        if model_name.split("_")[0] in model_config.values():
+            model_index = ix
+            model_path = os.path.join(model_dir, f"{model_name}_final.pth")
+            break
+
+    model = create_wavenet(config[model_index], device)
+    model.load_state_dict(torch.load(model_path, map_location=device)["model_state_dict"])
+
+    if input_file != None:
+        audio, sr = torchaudio.load(input_file)
+        if audio.shape[0] == 2:
+            audio = audio.mean(0, keepdim=True)
+    else:
+        audio = None
+    audio = audio.unsqueeze(0)
+    print(audio)
+
+    output = model.generate(initial_steps=audio)
+
+    return output, sr
+
 
 
 if __name__ == "__main__":
 
-    config = get_config()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    generated, sample_rate = generate_audio(model_name="m256k5v1", model_dir="./", input_file="./data/65.wav")
+    print(generated)
 
-    model = create_wavenet(config[0], device)
-    model.load_state_dict(torch.load("./m256k5v1_final.pth", map_location=device)["model_state_dict"])
-
-    path = "./test.wav"
-
-    # Load the audio
-    audio, sr = torchaudio.load(path)
-
-    if audio.shape[0] == 2:
-        audio = audio.mean(0, keepdim=True)
-
-    audio = audio.unsqueeze(0)
-    model.generate(audio, initial_steps=torch.ones(1, 1, 5000))
+    save_generated_audio(audio=generate_audio, path="./generated.wav", sr=sample_rate)

train.py
@@ -25,7 +25,7 @@ def train(config, device):
 
         model = create_wavenet(model_config, device)
 
-        train_dataloader, _, _ = create_dataloaders(model_config["pconv_output"])
+        train_dataloader, validation_dataloader, _ = create_dataloaders(model_config["pconv_output"])
 
         start_epoch = 0
         end_epoch = start_epoch + model_config["epochs"]
@@ -73,6 +73,9 @@ def train(config, device):
                 # Loss Backwards
                 loss.backward()
 
+                # Apply gradient clipping
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+
                 # Optimizer + LR_Scheduler step
                 optimizer.step()
                 scheduler.step()
@@ -86,11 +89,11 @@ def train(config, device):
 
             print(f"[INFO] Training Epoch: {epoch} | Loss: {epoch_loss:.4f} | Accuracy: {epoch_accuracy:.4f}%.")
 
-            if epoch % 3 == 0:
+            if epoch % 2 == 0:
                 # Do the validation
                 model.eval()
                 with torch.inference_mode():
-                    valid_batch_loader = tqdm(train_dataloader)
+                    valid_batch_loader = tqdm(validation_dataloader)
                     epoch_valid_loss = 0
                     epoch_valid_accuracy = 0
                     epoch_valid_step = 0

utils.py
@@ -8,7 +8,6 @@ from tqdm import tqdm
 
 from config import get_config
 
-
 def trim_audio(path : str, chunk_size : int = 5000):
     audio = AudioSegment.from_file(path)
     audio = audio.set_channels(1)
@@ -50,7 +49,7 @@ def sample_from_model(config : dict, path : str = None, length : int = 5, device
     return output
 
 def create_optimizer_and_scheduler(model, dataloader, start_epoch, end_epoch, optimizer_state_dict=None, scheduler_state_dict=None):
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001)
     scheduler = torch.optim.lr_scheduler.OneCycleLR(
         optimizer=optimizer,
         max_lr=0.05,
@@ -71,7 +70,7 @@ def calculate_accuracy(logits, targets):
     # apply softmax to the logits
     preds = F.softmax(logits, dim=-1).argmax(-1)
 
-    total += logits.shape[1]
+    total += logits.shape[1] * logits.shape[0]
     correct += (preds == targets).sum()
 
     accuracy = (correct / total) * 100
@@ -83,3 +82,9 @@ def create_summary_writer(model_name):
     writer = SummaryWriter(log_dir=writer_dir)
     print(f"[INFO] SummaryWriter created in {writer_dir}.")
     return writer
+
+def save_generated_audio(audio, path, sr):
+    if audio.ndim == 3:
+        audio = audio.squeeze(0) # squeeze the batch dimension
+    torchaudio.save(path, audio, sample_rate=sr)
+    print(f"[INFO] Audio sucessfully saved to: {path}.")

wavenet.py
@@ -32,13 +32,11 @@ class WaveNet(nn.Module):
         return x
 
     def generate(self,
-                 x,
                  initial_steps=None,
-                 num_steps=50,
-                 chunk_size=240000):
+                 num_steps=44100,
+                 chunk_size=22050):
 
         predictions = []
-
         if initial_steps == None:
             input_steps = torch.zeros((1, 1, chunk_size))
         else:
@@ -47,13 +45,14 @@ class WaveNet(nn.Module):
             elif initial_steps.shape[-1] < chunk_size:
                 zero_pad = torch.zeros((1, 1, chunk_size - initial_steps.shape[-1]))
                 input_steps = torch.cat([zero_pad, initial_steps], dim=-1)
+            else:
+                input_steps = initial_steps.unsqueeze(0)
 
         with torch.inference_mode():
             s = time.time()
             for step in range(num_steps):
                 # Do the forward pass
                 logits = self.forward(input_steps)
-
 
                 preds = F.softmax(logits, dim=-1)
                 arg_preds = greedy_decode(preds)